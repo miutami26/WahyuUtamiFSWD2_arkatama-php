77a824bfd9b41a5333c2fe473efb6f611a962626
Wahyu Utami <90756126+miutami26@users.noreply.github.com>
2023-04-14 12:26:12 +0700

    Add files via upload

---
+++ arkatama_php/biodatadiri.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Biodata Diri</title>
+    <style type="text/css">
+    body {
+        background-color: #f2f2f2;
+        font-family: sans-serif;
+    }
+
+    h1 {
+        text-align: center;
+        color: #333;
+    }
+
+    table {
+        margin: 0 auto;
+        border-collapse: collapse;
+        border: 1px solid #ccc;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    }
+
+    table td,
+    table th {
+        padding: 10px;
+        border: 1px solid #ccc;
+    }
+
+    img {
+        display: block;
+        margin: 0 auto;
+    }
+    </style>
+</head>
+
+<body>
+    <h1>Biodata Diri</h1>
+    <table>
+        <tr>
+            <th>Foto</th>
+            <td><img src="img/foto.jpg" alt="Foto" width="90" height="120"></td>
+        </tr>
+        <tr>
+            <th>Nama</th>
+            <td>Wahyu Utami</td>
+        </tr>
+        <tr>
+            <th>TTL</th>
+            <td>Ketapang, 26 Agustus 2002</td>
+        </tr>
+        <tr>
+            <th>Jenis Kelamin</th>
+            <td>Perempuan</td>
+        </tr>
+        <tr>
+            <th>Alamat</th>
+            <td>Jl. Brigjen Khatamso Kab. Ketapang</td>
+        </tr>
+        <tr>
+            <th>Riwayat Pendidikan</th>
+            <td>
+                <ul>
+                    <li>SDN 12 Sungai Melayu Rayak</li>
+                    <li>SMPN PGRI Sungai Melayu Rayak</li>
+                    <li>MAN 1 KETAPANG</li>
+                    <li>Politeknik Negeri Ketapang D3 (Teknologi Informasi) </li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th>Hobby</th>
+            <td>Membaca Novel</td>
+        </tr>
+    </table>
+</body>
+
+</html>
